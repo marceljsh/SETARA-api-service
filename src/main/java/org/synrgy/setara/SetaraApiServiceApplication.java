6dcfc6f5e78ba991f511c6cefa56bb896a34e811
alamsyhhh <alamsyah.rizki157@gmail.com>
2024-07-30 23:49:06 +0700

    change setaraapiserviceapplication

--- src/main/java/org/synrgy/setara/SetaraApiServiceApplication.java
+++ src/main/java/org/synrgy/setara/SetaraApiServiceApplication.java
@@ -1,6 +1,5 @@
 package org.synrgy.setara;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,12 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class SetaraApiServiceApplication {
 
 	public static void main(String[] args) {
-		// Load environment variables from .env file
-		Dotenv dotenv = Dotenv.load();
-
-		// Set System properties
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-
 		SpringApplication.run(SetaraApiServiceApplication.class, args);
 	}
+
 }
