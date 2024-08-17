289e1ca9b56956c8f699d4d2f3f578de13e9de19
aqilahakmalia <aqilaakmalia98@gmail.com>
2024-08-18 00:04:40 +0700

    remove unused import

--- src/main/java/org/synrgy/setara/transaction/service/JasperService.java
+++ src/main/java/org/synrgy/setara/transaction/service/JasperService.java
@@ -1,7 +1,5 @@
 package org.synrgy.setara.transaction.service;
 
-import org.synrgy.setara.transaction.dto.TransferResponse;
-import org.synrgy.setara.transaction.model.Transaction;
 import org.synrgy.setara.user.model.User;
 
 import java.util.UUID;
